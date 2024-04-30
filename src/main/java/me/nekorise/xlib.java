48b8428cd1697cfc2bc6673415e899619d5a7389
Nekorise <supercatura@gmail.com>
2024-04-30 18:38:25 +0500

    change version

---
+++ src/main/java/me/nekorise/xlib.java
@@ -0,0 +1,12 @@
+package me.nekorise;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class xlib extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        getLogger().info("xlib has started!");
+    }
+}
