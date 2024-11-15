ea9ead3b874119a006e789742066b2ae96de1134
Nekorise <supercatura@gmail.com>
2024-11-15 23:47:46 +0500

    1.1 (second try)
    - Add component list formating

--- src/main/java/me/nekorise/Hex/MMessage.java
+++ src/main/java/me/nekorise/Hex/MMessage.java
@@ -3,6 +3,9 @@ package me.nekorise.Hex;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MMessage
 {
     private static final MiniMessage miniMessage = MiniMessage.miniMessage();
@@ -16,7 +19,26 @@ public class MMessage
     public static Component applyColor(String text) {
         return miniMessage.deserialize(text);
     }
+
+    /**
+     * Applies color and formatting to the given text with ampersand (&) format support.
+     *
+     * @param text The text with ampersand format, e.g., "&aColored Text!"
+     * @return Component formatted text for use in Minecraft.
+     */
     public static Component applyColorWithAmpersand(String text) {
         return miniMessage.deserialize(HEX.applyColor(text));
+    }
+
+    /**
+     * Applies color and formatting to a list of strings.
+     *
+     * @param texts A list of texts with MiniMessage format, e.g., ["<#77b1de>Text1", "<#34a853>Text2"]
+     * @return A list of formatted Components for use in Minecraft.
+     */
+    public static List<Component> applyListColor(List<String> texts) {
+        return texts.stream()
+                .map(MMessage::applyColor)
+                .collect(Collectors.toList());
     }
 }
